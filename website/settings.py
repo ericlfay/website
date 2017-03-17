4387714dd88e9d4b2f59b5aec75f04d719f4e6c7
ericlfay <18708116154@163.com>
2017-03-17 12:32:16 +0800

    template debug false

--- website/settings.py
+++ website/settings.py
@@ -65,7 +65,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'debug': DEBUG,
         },
     },
 ]
