ec8424ffc6552443bc261fadf8ebba0d5e49593b
ericlfay <18708116154@163.com>
2017-03-20 16:48:42 +0800

    提交网页

--- captcha/views.py
+++ captcha/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 captcha_id = "a37c8a215b48279cdb65f10961af2bad"
 private_key = "233548502f29536550efec5a007451e2"
+
+
+def loginview(request):
+	'''user login view'''
+    return render(request, 'index.html')
