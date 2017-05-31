5f0d1f1f06aa99edd75edb9bdc471284b6d6a8ca
ericlfay <18708116154@163.com>
2017-05-31 11:46:29 +0800

    重新设计更新项目

--- captcha/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from .views import CaptchaView, SignUpValidatedView
-
-
-urlpatterns = [
-    url(r'^captcha/$', CaptchaView.as_view(),
-        name='get_captcha'),
-    url(r'^account/signup/$', SignUpValidatedView.as_view()),
-]
--- websiteenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/eric/Desktop/website/websiteenv/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
